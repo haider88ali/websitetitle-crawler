79157225a7aeab21128adcdecae58ae77e181d11
hashmi <4star@hashmi.local>
2025-05-13 01:01:29 +0500

    first commit

---
+++ async.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const async = require('async');
+
+
+function fetchTitle(address,callback){
+let fullUrl = address.startsWith('http') ? address : 'http://' + address;
+const lib = fullUrl.startsWith('https') ? https : http;
+
+    lib.get(fullUrl,(res) => {
+        let data = '';
+        res.on('data',chunk => data += chunk);
+        res.on('end' ,() => {
+            const match = data.match(/<title>([^<]*)<\/title>/i);
+            const title = match ? match[1] : 'NO RESPONSE';
+            callback(null,{url:address,title});
+
+        });
+    }).on('error',() => {
+        callback(null,{url:address,title:"No Url Found"});
+
+    })
+
+}
+
+http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+//   return res.send("here");
+    if (parsedUrl.pathname !== '/I/want/title') {
+        res.writeHead(404);
+        return res.end('<h1>404 Not Found</h1>');
+  }
+
+  const addresses = parsedUrl.query.address;
+  const list = Array.isArray(addresses) ? addresses : [addresses];
+  async.map(list,fetchTitle,(err,results) => {
+        res.writeHead(200,{'content-type' : 'text/html'})
+        res.write('<html><body><h1>Following are the titles of given websites:</h1><ul>');
+        results.forEach(r => res.write(`<li>${r.url} - " ${r.title}"`));
+        res.end('</ul></body></html>');
+  });
+}).listen(3000, () => console.log('Async version running at http://localhost:3000'));
